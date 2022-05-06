db6a3e50d36b940239445b1234e95feca11fc9b0
Carl Dea <carldea@gmail.com>
2022-05-06 11:43:11 -0400

    Update jextract_linux.sh
    Added glut when using jextract. Thanks to Meelis Müür who attended the workshop.
    Also, changed the environment variable from C_INCLUDE to C_INCLUDE_PATH to stay consistent with other script files.

--- lab5/jextract_linux.sh
+++ lab5/jextract_linux.sh
@@ -7,13 +7,16 @@ rm -rf src/sdl2
 ## Please change environment variables if libraries are installed else where
 
 # Linux
-export C_INCLUDE=/usr/include
+export C_INCLUDE_PATH=/usr/include
 export SDL_INCLUDE=/usr/include/SDL2
 
-jextract --source -d src -t sdl2 \
-    -I $C_INCLUDE \
+jextract --source \
+    -d src \
+    -t sdl2 \
+    -I $C_INCLUDE_PATH \
     -I $SDL_INCLUDE \
     -l SDL2 \
+    -l glut \
     --header-class-name LibSDL2 \
     sdlfoo.h
 
@@ -21,9 +24,9 @@ jextract --source -d src -t sdl2 \
 javac --add-modules=jdk.incubator.foreign -d classes src/sdl2/*.java
 
 jextract --source -d src -t org.unix \
-  -I $C_INCLUDE \
+  -I $C_INCLUDE_PATH \
   foo.h
 
 jextract -d classes -t org.unix \
-  -I $C_INCLUDE \
+  -I $C_INCLUDE_PATH \
   foo.h
