5d7d0b416c68ee87fa8c5fabe2c0031ba3a1e349
Carl Dea <carldea@gmail.com>
2022-05-18 15:39:32 -0400

    updated instructions for MacOS(intel) users for sdl2 installation

--- lab5/README.md
+++ lab5/README.md
@@ -6,6 +6,14 @@ This lab is primarily to have fun with a cross platform library that game develo
 
 Similar to Lab 4 lets build and run the apps before we do the exercise. Here are the following commands to build and run:
 
+## Assumptions
+Make sure you have SDL installed and the header files directory setup properly.
+Check out the [installs](../installs/README.md) for instructions. e.g. On MacOS(Intel) use the following:
+```shell
+brew install sdl2
+```
+**Note:** A known issue is when sdl2 version 2.0.22 is installed the headers directory don't work when using jextract. You'll need to download https://www.libsdl.org/release/SDL2-2.0.18.dmg and install headers from version 2.0.18. Please see: [installs](../installs/README.md)
+
 Linux
 ```shell
 bash jextract_linux.sh
--- lab5/jextract_macos.sh
+++ lab5/jextract_macos.sh
@@ -4,14 +4,29 @@ rm -rf generated/src
 rm -rf src/org
 rm -rf src/sdl2
 
+# Install the latest library with "brew install sdl2" to get the latest.
+# This will install the latest version such as 2.0.22, however when using jextract tool will need the headers from 2.0.18.
+#
+# To obtain Headers (include files directory for SDL2) and install:
+# Go to download https://www.libsdl.org/release/SDL2-2.0.18.dmg
+# Double click and drag or copy directory (SDL2.framework) folder
+#
+# Copy into in <Your home directory>/Library/Frameworks
+
+
 ## Please change environment variables if you are on Linux
 # MacOS
-export C_INCLUDE=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include
-export SDL_INCLUDE=/usr/local/include/SDL2
+export C_INCLUDE_PATH=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include
+
+# Download SDL's previous versions headers (directory) version 2.0.18
+export SDL_INCLUDE_PATH=$HOME/Library/Frameworks/SDL2.framework/Headers
+
+# The following gets an error: Invalid Cursor kind value: 602
+#export SDL_INCLUDE_PATH=/usr/local/Cellar/sdl2/2.0.22/include/SDL2
 
 jextract --source -d src -t sdl2 \
-    -I $C_INCLUDE \
-    -I $SDL_INCLUDE \
+    -I $C_INCLUDE_PATH \
+    -I $SDL_INCLUDE_PATH \
     -l SDL2 \
     --header-class-name LibSDL2 \
     sdlfoo.h
@@ -20,9 +35,9 @@ jextract --source -d src -t sdl2 \
 javac --add-modules=jdk.incubator.foreign -d classes src/sdl2/*.java
 
 jextract --source -d src -t org.unix \
-  -I $C_INCLUDE \
+  -I $C_INCLUDE_PATH \
   foo.h
 
 jextract -d classes -t org.unix \
-  -I $C_INCLUDE \
+  -I $C_INCLUDE_PATH \
   foo.h
