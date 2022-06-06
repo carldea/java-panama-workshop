7b383a04f66129739b6fe66d0e8169bd398225a9
Carl Dea <carldea@gmail.com>
2022-06-06 15:51:50 -0400

    Updated jextract Windows script for lab5

--- lab5/jextract_windows.cmd
+++ lab5/jextract_windows.cmd
@@ -1,32 +1,22 @@
 @echo off
 set OLDPATH=%PATH%
 
-## Please change environment variables libraries are installed elsewhere
-set path=C:\devtools\MinGW\bin;%PATH%
-set LIBRARY_PATH=C:\devtools\MinGW\lib
-set C_INCLUDE_PATH=C:\devtools\MinGW\include
+rem Please change environment variables libraries are installed elsewhere
+set PATH=C:\MinGW\bin;%PATH%
+set LIBRARY_PATH=C:\MinGW\lib
+set C_INCLUDE_PATH=C:\MinGW\include
 
-export C_INCLUDE=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include
-export SDL_INCLUDE=/usr/local/include/SDL2
+rem Generate SDL source
+jextract.exe --source -d src -t sdl2 -I %C_INCLUDE_PATH% -I %SDL_INCLUDE% -l SDL2 --header-class-name LibSDL2 sdlfoo.h
 
-# Linux
-# export C_INCLUDE=/usr/include
-# export SDL_INCLUDE=/usr/include/SDL2
+rem Generate SDL classes
+jextract.exe -d classes -t sdl2 -I %C_INCLUDE_PATH% -I %SDL_INCLUDE% -l SDL2 --header-class-name LibSDL2 sdlfoo.h
 
-jextract --source -d src -t sdl2 \
-    -I $C_INCLUDE \
-    -I $SDL_INCLUDE \
-    -l SDL2 \
-    --header-class-name LibSDL2 \
-    sdlfoo.h
+rem Generate Clib source
+jextract.exe --source -d src -t org.unix -I %C_INCLUDE_PATH% foo.h
 
-# add to classes (IMPORTANT: Notice namespace ..src/sdl2/..)
-javac --add-modules=jdk.incubator.foreign -d classes generated/src/sdl2/*.java
+rem Generate Clib classes
+jextract.exe -d classes -t org.unix -I %C_INCLUDE_PATH% foo.h
 
-jextract --source -d src -t org.unix \
-  -I $C_INCLUDE \
-  foo.h
-
-jextract -d classes -t org.unix \
-  -I $C_INCLUDE \
-  foo.h
+rem Compile example Java program
+javac --add-modules=jdk.incubator.foreign -d classes src/*.java
